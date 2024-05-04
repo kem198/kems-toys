6e1e36c9b0833d3c5ad6bcaf86142f4826d0c184
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-04 11:20:22 +0900

    add page hello-world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,6 +107,21 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+        <a
+          href="/hello-world"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Hello World{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+            Made 1st page and dynamic rooting.
+          </p>
+        </a>
       </div>
     </main>
   );
