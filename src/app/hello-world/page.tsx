8f0592fba6005202fd97b52a44ffafe4abcde4bb
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-05 00:27:51 +0900

    small changed

--- src/app/hello-world/page.tsx
+++ src/app/hello-world/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function HelloWorld() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World!</h1>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
     </main>
   );
 }
