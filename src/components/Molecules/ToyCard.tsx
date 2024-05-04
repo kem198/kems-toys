6d9e228bc794681d6be9663718e8041773670912
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-05 05:01:13 +0900

    small changed

--- src/components/Molecules/ToyCard.tsx
+++ src/components/Molecules/ToyCard.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function ToyCard({ title, description, link }) {
   return (
-    <div className="card w-48 bg-base-100 shadow-lg">
+    <div className="card w-48 border border-base-200 bg-base-100 shadow-lg">
       <Link href={link}>
         <figure>
           <svg
@@ -11,7 +11,7 @@ export default function ToyCard({ title, description, link }) {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="m-4 h-16 w-16 stroke-1"
+            className="m-8 h-16 w-16 stroke-1"
           >
             <path
               strokeLinecap="round"
@@ -20,7 +20,7 @@ export default function ToyCard({ title, description, link }) {
             />
           </svg>
         </figure>
-        <div className="card-body">
+        <div className="card-body pt-0">
           <h2 className="card-title">{title}</h2>
           <p>{description}</p>
         </div>
