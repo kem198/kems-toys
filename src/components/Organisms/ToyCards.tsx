66e332409fac062b865b8515aaca12cf212961cf
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-05 12:26:13 +0900

    make ToyCards

---
+++ src/components/Organisms/ToyCards.tsx
@@ -0,0 +1,27 @@
+import toysData from '@/assets/toys.json';
+import ToyCard from '@/components/Molecules/ToyCard';
+
+export default function ToyCards() {
+  // ToyCard コンポーネントをループ生成する関数
+  const generateToyCards = () =>
+    Object.keys(toysData).map((key) => {
+      const toy = toysData[key];
+      return (
+        <div key={key}>
+          <ToyCard
+            title={toy.title}
+            description={toy.description}
+            link={toy.link}
+            svgD={toy.svgD}
+          />
+        </div>
+      );
+    });
+
+  // ToyCard コンポーネントの配列を生成して返す
+  return (
+    <div className="flex flex-wrap justify-center gap-4 lg:justify-normal">
+      {generateToyCards()}
+    </div>
+  );
+}
