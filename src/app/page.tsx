4909b6108ec956b747a68ef0809c41de7684afe7
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-05 11:56:47 +0900

    toys.json からのループ処理を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,33 @@
 import ToyCard from '@/components/Molecules/ToyCard';
-import Toys from './toys.json';
+import ToysData from './toys.json';
+
+// Toysオブジェクトの型をアノテーション
+const Toys = ToysData;
 
 export default function Home() {
+  // ToyCardコンポーネントを生成する関数
+  const generateToyCards = () =>
+    Object.keys(Toys).map((key) => {
+      const toy = Toys[key];
+      return (
+        <div
+          className="flex flex-wrap justify-center gap-4 lg:justify-normal"
+          key={key}
+        >
+          <ToyCard
+            title={toy.title}
+            description={toy.description}
+            link={toy.link}
+            svgD={toy.svgD}
+          />
+        </div>
+      );
+    });
+
+  // ToyCardコンポーネントの配列を生成し、返す
   return (
     <div className="flex flex-wrap justify-center gap-4 lg:justify-normal">
-      <ToyCard
-        title={Toys.fizzBuzz.title}
-        description={Toys.fizzBuzz.description}
-        link={Toys.fizzBuzz.link}
-        svgD={Toys.fizzBuzz.svgD}
-      />
+      {generateToyCards()}
     </div>
   );
 }
