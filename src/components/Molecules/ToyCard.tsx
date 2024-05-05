380b536e2f9dfc3b68cafa5e547df4f159a5ee27
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-05 15:19:54 +0900

    strech clickable erea

--- src/components/Molecules/ToyCard.tsx
+++ src/components/Molecules/ToyCard.tsx
@@ -2,25 +2,26 @@ import Link from 'next/link';
 
 export default function ToyCard({ title, description, link, svgD }) {
   return (
-    <div className="card w-48 border border-base-200 bg-base-100 shadow-lg">
-      <Link href={link}>
-        <figure>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="m-8 h-16 w-16 stroke-1"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d={svgD} />
-          </svg>
-        </figure>
-        <div className="card-body p-6 pt-0">
-          <h2 className="card-title">{title}</h2>
-          <p className="prose prose-sm">{description}</p>
-        </div>
-      </Link>
-    </div>
+    <Link
+      className="card w-48 border border-base-200 bg-base-100 shadow-lg"
+      href={link}
+    >
+      <figure>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="m-8 h-16 w-16 stroke-1"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d={svgD} />
+        </svg>
+      </figure>
+      <div className="card-body p-6 pt-0">
+        <h2 className="card-title">{title}</h2>
+        <p className="prose prose-sm">{description}</p>
+      </div>
+    </Link>
   );
 }
