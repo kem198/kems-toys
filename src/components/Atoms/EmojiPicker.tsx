25ef4f39083c10b46ef95d851c1a274455604446
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-08 02:28:24 +0900

    add EmojiPicker

---
+++ src/components/Atoms/EmojiPicker.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Picker, { PickerProps } from 'emoji-picker-react';
+import { useState } from 'react';
+
+export default function EmojiPicker({
+  setter,
+}: {
+  setter: (text: string) => void;
+}) {
+  // 絵文字ピッカーの表示を切り替える変数とセッターを定義
+  const [showEmoji, setShowEmoji] = useState(false);
+
+  // 絵文字がクリックされたときに呼ばれる関数
+  const onEmojiClick: PickerProps['onEmojiClick'] = (emojiObject) => {
+    // 選択された絵文字を親コンポーネントのテキストへ追加する
+    setter((prevText) => prevText + ` ${emojiObject.emoji}`);
+  };
+
+  return (
+    <div>
+      {/* 絵文字ピッカーの表示を切り替えるボタン */}
+      <button
+        type="button"
+        className="btn btn-circle text-lg"
+        onClick={() => setShowEmoji(!showEmoji)}
+      >
+        😂
+      </button>
+      {/* showEmoji の条件付きで絵文字ピッカーをレンダリングする */}
+      {showEmoji && <Picker onEmojiClick={onEmojiClick} className="mt-2" />}
+    </div>
+  );
+}
