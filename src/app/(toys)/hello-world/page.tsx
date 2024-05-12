35e30052b7d74b8fb2bc14660a01f936f697d961
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-12 17:20:33 +0900

    Revert "add loading test page"
    This reverts commit 68787dc68541bc0b00cbf06385adcdb2b60c9586.

--- src/app/(toys)/hello-world/page.tsx
+++ src/app/(toys)/hello-world/page.tsx
@@ -12,9 +12,6 @@ export default function HelloWorld() {
         <li>
           <Link href="/hello-world/md/">Markdown</Link>
         </li>
-        <li>
-          <Link href="/hello-world/loading/">Loading</Link>
-        </li>
       </ul>
     </article>
   );
