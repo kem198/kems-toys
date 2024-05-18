d3013c08af3c06cece7023cce8e2c53bb24ed997
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-18 20:16:24 +0900

    refactoring

--- src/utilities/requestDeviceMotionPermission.ts
+++ src/utilities/requestDeviceMotionPermission.ts
@@ -1,22 +1,19 @@
 // iOS でのみ存在する requestPermission メソッドの型を拡張する
-interface DeviceOrientationEventiOS extends DeviceOrientationEvent {
+interface DeviceOrientationEventIOS extends DeviceOrientationEvent {
   requestPermission?: () => Promise<'granted' | 'denied'>;
 }
 
 // iOS 13 でユーザーに "動作と方向" へのアクセス許可を求める関数
 async function requestDeviceMotionPermission() {
-  // DeviceOrientationEventiOS を介して直接 requestPermission メソッドを取り出す
+  // DeviceOrientationEventIOS を介して直接 requestPermission メソッドを取り出す
   const { requestPermission } =
-    DeviceOrientationEvent as unknown as DeviceOrientationEventiOS;
+    DeviceOrientationEvent as unknown as DeviceOrientationEventIOS;
 
-  try {
-    if (requestPermission) {
-      await requestPermission(); // iOS Safari でのみ存在する関数があれば実行
-    } else {
-      throw new Error('DeviceMotionEvent.requestPermission is not found');
-    }
-  } catch (error) {
-    alert(error.message); // エラーメッセージをアラートで表示
+  if (requestPermission) {
+    // iOS Safari でのみ存在する関数があれば実行する
+    await requestPermission();
+  } else {
+    alert('DeviceMotionEvent.requestPermission is not found');
   }
 }
 
