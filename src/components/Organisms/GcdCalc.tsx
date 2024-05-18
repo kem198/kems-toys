f47aa1e563884d63bffd3b2fafd2d3ba1d975322
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-18 09:55:38 +0900

    refactoring

--- src/components/Organisms/GcdCalc.tsx
+++ src/components/Organisms/GcdCalc.tsx
@@ -11,26 +11,27 @@ import { useState } from 'react';
  */
 const GcdCalc = () => {
   // state 変数とセッターを定義
-  const [mCount, setMCount] = useState<number | null>(null);
-  const [nCount, setNCount] = useState<number | null>(null);
+  const [aCount, setACount] = useState<number | null>(null);
+  const [bCount, setBCount] = useState<number | null>(null);
 
   /**
    * m と n をリセットする関数
    */
   const resetCounts = () => {
-    setMCount(null);
-    setNCount(null);
+    setACount(null);
+    setBCount(null);
   };
 
   return (
     <div className="container my-8 flex w-fit flex-col gap-4 max-lg:mx-auto">
-      <LabeledNumberInput labelText="a =" count={mCount} setCount={setMCount} />
-      <LabeledNumberInput labelText="b =" count={nCount} setCount={setNCount} />
+      <LabeledNumberInput labelText="a =" count={aCount} setCount={setACount} />
+      <LabeledNumberInput labelText="b =" count={bCount} setCount={setBCount} />
       <ResultDisplay>
-        m = {mCount !== null && nCount !== null && calcGcd(mCount, nCount)}
+        m = {aCount !== null && bCount !== null && calcGcd(aCount, bCount)}
       </ResultDisplay>
       <ResetButton onClick={resetCounts} />
     </div>
   );
 };
+
 export { GcdCalc };
--- src/utilities/gcd.ts
+++ src/utilities/gcd.ts
@@ -1,36 +1,33 @@
 /**
  * 最大公約数を計算する関数
  *
- * @param {number} m - 自然数 m
- * @param {number} n - 自然数 n
- * @returns {number | null} - 最大公約数を返す。入力が不正である場合は null を返す
+ * @param {number} m - 整数 m
+ * @param {number} n - 整数 n
+ * @returns {number} - 最大公約数を返す。両方の入力が 0 の場合は null を返す
  * @see https://ja.wikipedia.org/wiki/ユークリッドの互除法
  */
-const calcGcd = (m: number, n: number): number | null => {
-  // 入力が自然数であるか評価する
-  if (m <= 0 || n <= 0) {
+const calcGcd = (a: number, b: number): number => {
+  // a と b が共に 0 の場合は定義されないので処理を中断する
+  if (a === 0 && b === 0) {
     return null;
-  }
-  // より大きい値を TempM, 小さい値を TempN へ代入する
-  let tempM = Math.max(m, n);
-  let tempN = Math.min(m, n);
-
-  while (tempN !== 0) {
-    // m と n の剰余を求める
-    const q = tempM % tempN;
-
-    // // m と n の商を求める
-    // const r = Math.floor(tempM / tempN);
-    // // 表示
-    // console.log(`${tempM} / ${tempN} = ${r} ... ${q}`);
-
-    // 元の n を新たに m とする
-    tempM = tempN;
-    // m と n の剰余を新たに n とする
-    tempN = q;
+    // throw new Error('両方の入力が0の場合、最大公約数は定義されません。');
   }
 
-  return tempM;
+  // 負の値を考慮して絶対値を取る
+  const AbsA = Math.abs(a);
+  const AbsB = Math.abs(b);
+
+  // より大きい値を m, 小さい値を n へ代入する
+  let m = Math.max(AbsA, AbsB);
+  let n = Math.min(AbsA, AbsB);
+
+  while (n !== 0) {
+    const q = m % n;
+    m = n;
+    n = q;
+  }
+
+  return m;
 };
 
 export { calcGcd };
