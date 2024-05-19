03705e68f1da57dac730d978f6605cf5ff268134
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-19 18:21:47 +0900

    mod

--- src/app/(contents)/(toys)/modane-3d/components/ModaneThree.tsx
+++ src/app/(contents)/(toys)/modane-3d/components/ModaneThree.tsx
@@ -22,14 +22,26 @@ const FallbackComponent = () => (
 );
 
 const ModaneThree = () => (
-  <div id="canvas-container" style={{ width: '80vw', height: '80vh' }}>
-    <Canvas>
+  <div className="flex justify-center">
+    <Canvas
+      camera={{ fov: 30, near: 0.1, far: 2000, position: [40, 20, 0] }}
+      style={{ width: '90vw', height: '80vh' }}
+      className="border"
+    >
       <ambientLight intensity={1} />
       <directionalLight color="white" position={[0, 0, 5]} />
       <Suspense fallback={<FallbackComponent />}>
         <Model />
       </Suspense>
-      <OrbitControls enableZoom enableRotate />
+      <OrbitControls
+        enableZoom
+        enableRotate
+        minPolarAngle={Math.PI / 6} // 最小の緯度角（ラジアン単位）
+        maxPolarAngle={Math.PI / 1} // 最大の緯度角（ラジアン単位）
+        minDistance={0.5} // カメラとターゲットの最小距離
+        maxDistance={4} // カメラとターゲットの最大距離
+        target={[0, 0.4, 0]} // カメラの注視点を設定
+      />
     </Canvas>
   </div>
 );
