967ac09d34da99bfd3d719489e64c10a4bc32d9e
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-19 19:33:10 +0900

    small changed

--- src/assets/toys.ts
+++ src/assets/toys.ts
@@ -47,7 +47,7 @@ const toys: Props = {
   modane3d: {
     title: 'もだねちゃん 3D ビューワー',
     description: 'もだねちゃん、デジタルの世界へ',
-    link: '/modane-3d',
+    link: '/modane-3d-viewer',
     svgD: 'm21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9',
   },
 };
