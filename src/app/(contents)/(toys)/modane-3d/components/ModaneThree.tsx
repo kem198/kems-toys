b19d41f9e5d7655125d8b3b53b958b5dcdfe8b44
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-19 18:28:16 +0900

    fix comments

--- src/app/(contents)/(toys)/modane-3d/components/ModaneThree.tsx
+++ src/app/(contents)/(toys)/modane-3d/components/ModaneThree.tsx
@@ -9,12 +9,16 @@ import { GLTFLoader } from 'three/examples/jsm/Addons';
  * モデルの読み込みを行う関数
  */
 const Model = () => {
+  // GLTFLoaderを使用してモデルを読み込む
   const result = useLoader(GLTFLoader, '/assets/modane.glb');
-  console.log(result);
+
+  // ロードされたモデルを表示する
   return <primitive object={result.scene} />;
 };
 
-// 必要に応じてフォールバックコンポーネントを定義
+/**
+ * モデルのロード中に表示する関数
+ */
 const FallbackComponent = () => (
   <Html>
     <div>Loading...</div>
@@ -23,24 +27,29 @@ const FallbackComponent = () => (
 
 const ModaneThree = () => (
   <div className="flex justify-center">
+    {/* シーンの設定 */}
     <Canvas
       camera={{ fov: 30, near: 0.1, far: 2000, position: [40, 20, 0] }}
       style={{ width: '90vw', height: '80vh' }}
       className="border"
     >
+      {/* 半球光源 (環境光) */}
       <ambientLight intensity={1} />
+      {/* 平行光源 */}
       <directionalLight color="white" position={[0, 0, 5]} />
+      {/* モデルを非同期で読み込む */}
       <Suspense fallback={<FallbackComponent />}>
         <Model />
       </Suspense>
+      {/* カメラ制御 */}
       <OrbitControls
         enableZoom
         enableRotate
-        minPolarAngle={Math.PI / 6} // 最小の緯度角（ラジアン単位）
-        maxPolarAngle={Math.PI / 1} // 最大の緯度角（ラジアン単位）
+        minPolarAngle={Math.PI / 6} // 最小の緯度角 (ラジアン単位)
+        maxPolarAngle={Math.PI / 1} // 最大の緯度角 (ラジアン単位)
         minDistance={0.5} // カメラとターゲットの最小距離
         maxDistance={4} // カメラとターゲットの最大距離
-        target={[0, 0.4, 0]} // カメラの注視点を設定
+        target={[0, 0.4, 0]} // カメラの注視点
       />
     </Canvas>
   </div>
