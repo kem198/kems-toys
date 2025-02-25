37aca9fac6960401a164814326b1fbe63e157378
kem198 <kenkenpa198@gmail.com>
2025-02-26 03:34:09 +0900

    refactor: simplify ToyCards component by using Object.entries for mapping

--- src/components/Organisms/ToyCards.tsx
+++ src/components/Organisms/ToyCards.tsx
@@ -1,28 +1,18 @@
 import { ToyCard } from "@/components/Molecules/ToyCard";
 import { TOYS } from "@/constants/toys";
 
-const ToyCards = () => {
-  // ToyCard コンポーネントをループ生成する関数
-  const generateToyCards = () =>
-    Object.keys(TOYS).map((key) => {
-      const toy = TOYS[key];
-      return (
-        <ToyCard
-          key={key}
-          title={toy.title}
-          description={toy.description}
-          link={toy.link}
-          svgD={toy.svgD}
-        />
-      );
-    });
-
-  // ToyCard コンポーネントの配列を生成して返す
-  return (
-    <div className="flex flex-wrap justify-center gap-4 lg:justify-normal">
-      {generateToyCards()}
-    </div>
-  );
-};
+const ToyCards = () => (
+  <div className="flex flex-wrap justify-center gap-4 lg:justify-normal">
+    {Object.entries(TOYS).map(([key, toy]) => (
+      <ToyCard
+        key={key}
+        title={toy.title}
+        description={toy.description}
+        link={toy.link}
+        svgD={toy.svgD}
+      />
+    ))}
+  </div>
+);
 
 export { ToyCards };
