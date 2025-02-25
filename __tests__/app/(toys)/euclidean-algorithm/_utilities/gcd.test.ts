c06e7fd22252cc71e4da72c4982937166b7c1244
kem198 <kenkenpa198@gmail.com>
2025-02-26 05:12:10 +0900

    test: add unit tests for calcGcdSteps function with various input scenarios

--- __tests__/app/(toys)/euclidean-algorithm/_utilities/gcd.test.ts
+++ __tests__/app/(toys)/euclidean-algorithm/_utilities/gcd.test.ts
@@ -1,4 +1,7 @@
-import { calcGcd } from "@/app/(toys)/euclidean-algorithm/_utilities/gcd";
+import {
+  calcGcd,
+  calcGcdSteps,
+} from "@/app/(toys)/euclidean-algorithm/_utilities/gcd";
 import { describe, expect, it } from "vitest";
 
 describe("calcGcd", () => {
@@ -24,6 +27,38 @@ describe("calcGcd", () => {
 
   it("should throw an error if the numbers are 0 and 0", () => {
     expect(() => calcGcd(0, 0)).toThrow(
+      Error("両方の入力が 0 の場合、最大公約数は定義されません"),
+    );
+  });
+});
+
+describe("calcGcdSteps", () => {
+  it("should return correct steps if the numbers are 2 and 3", () => {
+    expect(calcGcdSteps(2, 3)).toEqual([
+      "2 ÷ 3 = 0 ... 2",
+      "3 ÷ 2 = 1 ... 1",
+      "2 ÷ 1 = 2 ... 0",
+    ]);
+  });
+
+  it("should return correct steps if the numbers are 2 and 6", () => {
+    expect(calcGcdSteps(2, 6)).toEqual(["2 ÷ 6 = 0 ... 2", "6 ÷ 2 = 3 ... 0"]);
+  });
+
+  it("should return correct steps if the numbers are 3 and 6", () => {
+    expect(calcGcdSteps(3, 6)).toEqual(["3 ÷ 6 = 0 ... 3", "6 ÷ 3 = 2 ... 0"]);
+  });
+
+  it("should return correct steps if the numbers are -3 and 6", () => {
+    expect(calcGcdSteps(-3, 6)).toEqual(["3 ÷ 6 = 0 ... 3", "6 ÷ 3 = 2 ... 0"]);
+  });
+
+  it("should return correct steps if the numbers are -6 and -6", () => {
+    expect(calcGcdSteps(-6, -6)).toEqual(["6 ÷ 6 = 1 ... 0"]);
+  });
+
+  it("should throw an error if the numbers are 0 and 0", () => {
+    expect(() => calcGcdSteps(0, 0)).toThrow(
       Error("両方の入力が 0 の場合、最大公約数は定義されません"),
     );
   });
