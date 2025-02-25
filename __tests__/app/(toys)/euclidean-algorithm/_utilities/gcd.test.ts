816a1b0e27ab2f0875bc01579ccf5c24a212249a
kem198 <kenkenpa198@gmail.com>
2025-02-26 04:53:12 +0900

    test: add unit tests for calcGcd function

---
+++ __tests__/app/(toys)/euclidean-algorithm/_utilities/gcd.test.ts
@@ -0,0 +1,26 @@
+import { calcGcd } from "@/app/(toys)/euclidean-algorithm/_utilities/gcd";
+import { describe, expect, it } from "vitest";
+
+describe("calcGcd", () => {
+  it("should return 1 if the numbers are 1 and 1", () => {
+    expect(calcGcd(1, 1)).toBe(1);
+  });
+
+  it("should return 3 if the numbers are 6 and 9", () => {
+    expect(calcGcd(6, 9)).toBe(3);
+  });
+
+  it("should return 3 if the numbers are -6 and 9", () => {
+    expect(calcGcd(-6, 9)).toBe(3);
+  });
+
+  it("should return 3 if the numbers are -6 and -9", () => {
+    expect(calcGcd(-6, -9)).toBe(3);
+  });
+
+  it("should throw an error if the numbers are 0 and 0", () => {
+    expect(() => calcGcd(0, 0)).toThrow(
+      Error("両方の入力が 0 の場合、最大公約数は定義されません"),
+    );
+  });
+});
