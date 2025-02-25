dd27047261b51a0835b9421237b8a36b5d5fbbb8
kem198 <kenkenpa198@gmail.com>
2025-02-26 05:32:08 +0900

    test: categorize gcdSteps tests for clarity and add new cases for relatively prime numbers

--- __tests__/app/(toys)/euclidean-algorithm/_utilities/gcd.test.ts
+++ __tests__/app/(toys)/euclidean-algorithm/_utilities/gcd.test.ts
@@ -78,62 +78,66 @@ describe("calcGcd", () => {
 
 describe("calcGcdSteps", () => {
   describe("when both numbers are positive", () => {
-    it("should return correct steps if the numbers are 2 and 3", () => {
-      expect(calcGcdSteps(2, 3)).toEqual([
-        "2 ÷ 3 = 0 ... 2",
-        "3 ÷ 2 = 1 ... 1",
-        "2 ÷ 1 = 2 ... 0",
-      ]);
+    describe("and relatively prime", () => {
+      it("should return correct steps if the numbers are 2 and 3", () => {
+        expect(calcGcdSteps(2, 3)).toEqual([
+          "2 ÷ 3 = 0 ... 2",
+          "3 ÷ 2 = 1 ... 1",
+          "2 ÷ 1 = 2 ... 0",
+        ]);
+      });
+
+      it("should return correct steps if the numbers are 17 and 19", () => {
+        expect(calcGcdSteps(17, 19)).toEqual([
+          "17 ÷ 19 = 0 ... 17",
+          "19 ÷ 17 = 1 ... 2",
+          "17 ÷ 2 = 8 ... 1",
+          "2 ÷ 1 = 2 ... 0",
+        ]);
+      });
+
+      it("should return correct steps if the numbers are 14 and 25", () => {
+        expect(calcGcdSteps(14, 25)).toEqual([
+          "14 ÷ 25 = 0 ... 14",
+          "25 ÷ 14 = 1 ... 11",
+          "14 ÷ 11 = 1 ... 3",
+          "11 ÷ 3 = 3 ... 2",
+          "3 ÷ 2 = 1 ... 1",
+          "2 ÷ 1 = 2 ... 0",
+        ]);
+      });
     });
 
-    it("should return correct steps if the numbers are 2 and 6", () => {
-      expect(calcGcdSteps(2, 6)).toEqual([
-        "2 ÷ 6 = 0 ... 2",
-        "6 ÷ 2 = 3 ... 0",
-      ]);
-    });
+    describe("and not relatively prime", () => {
+      it("should return correct steps if the numbers are 2 and 6", () => {
+        expect(calcGcdSteps(2, 6)).toEqual([
+          "2 ÷ 6 = 0 ... 2",
+          "6 ÷ 2 = 3 ... 0",
+        ]);
+      });
 
-    it("should return correct steps if the numbers are 3 and 6", () => {
-      expect(calcGcdSteps(3, 6)).toEqual([
-        "3 ÷ 6 = 0 ... 3",
-        "6 ÷ 3 = 2 ... 0",
-      ]);
-    });
+      it("should return correct steps if the numbers are 3 and 6", () => {
+        expect(calcGcdSteps(3, 6)).toEqual([
+          "3 ÷ 6 = 0 ... 3",
+          "6 ÷ 3 = 2 ... 0",
+        ]);
+      });
 
-    it("should return correct steps if the numbers are 123456 and 7890", () => {
-      expect(calcGcdSteps(123456, 7890)).toEqual([
-        "123456 ÷ 7890 = 15 ... 5106",
-        "7890 ÷ 5106 = 1 ... 2784",
-        "5106 ÷ 2784 = 1 ... 2322",
-        "2784 ÷ 2322 = 1 ... 462",
-        "2322 ÷ 462 = 5 ... 12",
-        "462 ÷ 12 = 38 ... 6",
-        "12 ÷ 6 = 2 ... 0",
-      ]);
-    });
+      it("should return correct steps if the numbers are 123456 and 7890", () => {
+        expect(calcGcdSteps(123456, 7890)).toEqual([
+          "123456 ÷ 7890 = 15 ... 5106",
+          "7890 ÷ 5106 = 1 ... 2784",
+          "5106 ÷ 2784 = 1 ... 2322",
+          "2784 ÷ 2322 = 1 ... 462",
+          "2322 ÷ 462 = 5 ... 12",
+          "462 ÷ 12 = 38 ... 6",
+          "12 ÷ 6 = 2 ... 0",
+        ]);
+      });
 
-    it("should return correct steps if the numbers are 17 and 19", () => {
-      expect(calcGcdSteps(17, 19)).toEqual([
-        "17 ÷ 19 = 0 ... 17",
-        "19 ÷ 17 = 1 ... 2",
-        "17 ÷ 2 = 8 ... 1",
-        "2 ÷ 1 = 2 ... 0",
-      ]);
-    });
-
-    it("should return correct steps if the numbers are 14 and 25", () => {
-      expect(calcGcdSteps(14, 25)).toEqual([
-        "14 ÷ 25 = 0 ... 14",
-        "25 ÷ 14 = 1 ... 11",
-        "14 ÷ 11 = 1 ... 3",
-        "11 ÷ 3 = 3 ... 2",
-        "3 ÷ 2 = 1 ... 1",
-        "2 ÷ 1 = 2 ... 0",
-      ]);
-    });
-
-    it("should return correct steps if the numbers are 25 and 5", () => {
-      expect(calcGcdSteps(25, 5)).toEqual(["25 ÷ 5 = 5 ... 0"]);
+      it("should return correct steps if the numbers are 25 and 5", () => {
+        expect(calcGcdSteps(25, 5)).toEqual(["25 ÷ 5 = 5 ... 0"]);
+      });
     });
   });
 
