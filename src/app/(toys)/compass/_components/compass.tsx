a061dd6f8c0f5022476b491a8f9412119230dc1a
kenkenpa198 <kenkenpa198@gmail.com>
2025-06-01 22:18:28 +0900

    feat: refactor Compass component to improve structure and maintainability

--- src/app/(toys)/compass/_components/compass.tsx
+++ src/app/(toys)/compass/_components/compass.tsx
@@ -1,18 +1,27 @@
 "use client";
 
+import { DeviceOrientationRequestPermissionButton } from "@/components/atoms/device-orientation-request-permission-button";
 import { useDeviceOrientation } from "@/hooks/use-device-orientation";
+import { requestDeviceMotionPermission } from "@/utilities/request-device-motion-permission";
 
 const Compass = () => {
   const { alpha } = useDeviceOrientation();
 
   return (
-    <div
-      className="radial-progress m-4 text-center"
-      style={{ "--value": 100 - (alpha / 360) * 100 }}
-      role="progressbar"
-    >
-      {Math.trunc(alpha)}
-    </div>
+    <>
+      <div
+        className="radial-progress m-4 text-center"
+        style={{ "--value": 100 - (alpha / 360) * 100 } as React.CSSProperties}
+        role="progressbar"
+      >
+        {Math.trunc(alpha)}
+      </div>
+      <div>
+        <DeviceOrientationRequestPermissionButton
+          onClick={requestDeviceMotionPermission}
+        />
+      </div>
+    </>
   );
 };
 
