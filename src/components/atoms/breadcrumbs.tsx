7f96daaa82206d15ee9b4cb628783901bccbcd28
KeM198 <kenkenpa198@gmail.com>
2025-10-30 07:34:37 +0900

    refactor: replace div and ul with Breadcrumb components for improved semantics

--- src/components/atoms/breadcrumbs.tsx
+++ src/components/atoms/breadcrumbs.tsx
@@ -2,6 +2,13 @@
 
 import Link from "next/link";
 import { useSelectedLayoutSegments } from "next/navigation";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "../ui/breadcrumb";
 
 const Breadcrumbs = () => {
   // 呼び出されたページまでのセグメントを配列として取得する
@@ -13,32 +20,39 @@ const Breadcrumbs = () => {
   );
 
   return (
-    <div className="breadcrumbs pb-4 pt-0 text-sm text-stone-600">
-      <ul>
+    <Breadcrumb>
+      <BreadcrumbList>
         {/* ホームへのリンクは静的に作成する */}
-        <li>
-          <Link href="/">Home</Link>
-        </li>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="/">Home</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
         {/* セグメントを map で繰り返し生成 */}
         {pathnamesIgnoredRouteGroups.map((segment, index) => (
-          <li key={segment}>
-            {/**
-             * 最後のセグメント以外はリンクとして表示する
-             * 最後のセグメントはテキストとして表示する
-             */}
-            {index === pathnamesIgnoredRouteGroups.length - 1 ? (
-              <span className="text-stone-400">{segment}</span>
-            ) : (
-              <Link
-                href={`/${pathnamesIgnoredRouteGroups.slice(0, index + 1).join("/")}`}
-              >
-                {segment}
-              </Link>
-            )}
-          </li>
+          <>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem key={segment}>
+              {/**
+               * 最後のセグメント以外はリンクとして表示する
+               * 最後のセグメントはテキストとして表示する
+               */}
+              {index === pathnamesIgnoredRouteGroups.length - 1 ? (
+                <span className="text-stone-400">{segment}</span>
+              ) : (
+                <BreadcrumbLink asChild>
+                  <Link
+                    href={`/${pathnamesIgnoredRouteGroups.slice(0, index + 1).join("/")}`}
+                  >
+                    {segment}
+                  </Link>
+                </BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+          </>
         ))}
-      </ul>
-    </div>
+      </BreadcrumbList>
+    </Breadcrumb>
   );
 };
 
