b7516e8430f9edf19906296526f28428a66215e4
KeM198 <kenkenpa198@gmail.com>
2025-10-31 21:31:09 +0900

    refactor: add whitespace handling to article for improved text display

--- src/app/(toys)/layout.tsx
+++ src/app/(toys)/layout.tsx
@@ -8,7 +8,8 @@ function ToysLayout({
   return (
     <div className="flex flex-col gap-2">
       <Breadcrumbs />
-      <article className="prose max-w-2xl prose-blockquote:font-normal">
+      {/* TODO: 表示崩れするので prose-pre:whitespace-pre-wrap を指定している */}
+      <article className="prose max-w-2xl prose-blockquote:font-normal prose-pre:whitespace-pre-wrap">
         {children}
       </article>
     </div>
