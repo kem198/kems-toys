3b66570957b06c3672b48d90d907e7791e4c44a1
KeM198 <kenkenpa198@gmail.com>
2025-11-03 16:01:01 +0900

    refactor: Remove unused types and functions from birthday message component for cleaner code

--- src/app/(toys)/etrian-calendar/_features/registry/components/birthday-message.tsx
+++ src/app/(toys)/etrian-calendar/_features/registry/components/birthday-message.tsx
@@ -1,54 +1,11 @@
+import { Etrian } from "@/app/(toys)/etrian-calendar/_common/types/etrian";
 import {
-  Etrian,
-  EtrianDay,
-  EtrianMonthName,
-  EtrianNewYearsEveName,
-} from "@/app/(toys)/etrian-calendar/_common/types/etrian";
-import {
+  getDiffDaysBetweenSolarAndEtrianDate,
   toEtrianDate,
-  toSolarDate,
 } from "@/app/(toys)/etrian-calendar/_common/utils/etrian-utils";
-import { MILLISECONDS_PER_DAY } from "@/constants/date";
 
 type BirthdayMessageProps = {
   etrian: Etrian;
-};
-
-const getDiffDaysBetweenSolarAndEtrianDate = (
-  solarDate: Date,
-  etrianDate: {
-    month: EtrianMonthName | EtrianNewYearsEveName;
-    day: EtrianDay;
-  },
-): number => {
-  // 時刻の影響を避けるため「太陽暦 00:00:00」を基準にする
-  const solarDateMidnight = new Date(
-    solarDate.getFullYear(),
-    solarDate.getMonth(),
-    solarDate.getDate(),
-  );
-
-  // 太陽暦の年を割り当てて今年の世界樹歴を作成する
-  let targetEtrianDate = toSolarDate({
-    year: solarDateMidnight.getFullYear(),
-    month: etrianDate.month,
-    day: etrianDate.day,
-  });
-
-  // 今年の世界樹歴が過去なら翌年の日付にする
-  if (targetEtrianDate.getTime() < solarDateMidnight.getTime()) {
-    targetEtrianDate = toSolarDate({
-      year: solarDateMidnight.getFullYear() + 1,
-      month: etrianDate.month,
-      day: etrianDate.day,
-    });
-  }
-
-  const diffMilliseconds =
-    targetEtrianDate.getTime() - solarDateMidnight.getTime();
-  const diffDays = Math.ceil(diffMilliseconds / MILLISECONDS_PER_DAY);
-
-  return diffDays;
 };
 
 export function BirthdayMessage({ etrian }: BirthdayMessageProps) {
