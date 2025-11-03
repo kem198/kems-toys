e802d3c375eec543500d6e83d48c5dbf6380295f
KeM198 <kenkenpa198@gmail.com>
2025-11-03 16:31:51 +0900

    feat: Add sampleEtrians data for testing and development purposes

---
+++ src/app/(toys)/etrian-calendar/_common/constants/sample.ts
@@ -0,0 +1,37 @@
+import { Etrian } from "@/app/(toys)/etrian-calendar/_common/types/etrian";
+
+export const sampleEtrians: Etrian[] = [
+  {
+    id: "sample-medic",
+    name: "メディ子",
+    dateOfBirth: {
+      month: "皇帝ノ月",
+      day: 1,
+    },
+    affiliations: ["アトラス", "エトリア"],
+    orderNum: 0,
+    memo: "ずうずうしい！",
+  },
+  {
+    id: "sample-paladin",
+    name: "ししょー",
+    dateOfBirth: {
+      month: "皇帝ノ月",
+      day: 1,
+    },
+    affiliations: ["アトラス", "エトリア"],
+    orderNum: 0,
+    memo: "ウルトラCだろう…私もそう思う",
+  },
+  {
+    id: "sample-gunner",
+    name: "ガン子",
+    dateOfBirth: {
+      month: "皇帝ノ月",
+      day: 1,
+    },
+    affiliations: ["アトラス", "ハイ・ラガード"],
+    orderNum: 0,
+    memo: "私のフィギュアで3倍売れる",
+  },
+];
