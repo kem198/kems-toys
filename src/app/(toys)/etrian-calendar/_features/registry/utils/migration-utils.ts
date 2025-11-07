7eb0081114bce491f97a150e5095543d98a7edf3
kem198 <kenkenpa198@gmail.com>
2025-11-07 19:46:53 +0900

    refactor: remove console.log statement from migrateEtrians function

--- src/app/(toys)/etrian-calendar/_features/registry/utils/migration-utils.ts
+++ src/app/(toys)/etrian-calendar/_features/registry/utils/migration-utils.ts
@@ -4,7 +4,6 @@ import {
 } from "@/app/(toys)/etrian-calendar/_common/types/etrian";
 
 export const migrateEtrians = (etrianV1s: EtrianV1[]): Etrian[] => {
-  console.log(etrianV1s);
   const migratedEtrians: Etrian[] = [
     {
       id: etrianV1s[0].id,
