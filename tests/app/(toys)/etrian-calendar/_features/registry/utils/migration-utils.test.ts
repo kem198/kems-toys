8fc286be971fe94e42de363bad19e1caaef55469
kem198 <kenkenpa198@gmail.com>
2025-11-07 20:47:34 +0900

    feat: add test for empty EtrianV1 array and update dateOfBirth handling in migration-utils tests

--- tests/app/(toys)/etrian-calendar/_features/registry/utils/migration-utils.test.ts
+++ tests/app/(toys)/etrian-calendar/_features/registry/utils/migration-utils.test.ts
@@ -4,6 +4,17 @@ import { describe, expect, it } from "vitest";
 
 describe("migration-utils tests", () => {
   describe("migrateEtrians() tests", () => {
+    it("'EtrianV1[] が空配列のとき、Etrian[] の空配列を返すこと", () => {
+      // Arrange
+      const oldEtrians: EtrianV1[] = [];
+
+      // Act
+      const migratedEtrians = migrateEtrians(oldEtrians);
+
+      // Assert
+      expect(migratedEtrians).toEqual([]);
+    });
+
     it("'EtrianV1[] を渡すと Etrian[] へ変換されること (dateOfMonth.month/day あり -> そのまま)", () => {
       // Arrange
       const oldEtrians: EtrianV1[] = [
@@ -118,7 +129,7 @@ describe("migration-utils tests", () => {
         {
           id: "test-id-0",
           name: "セトハ",
-          dateOfBirth: { month: "笛鼠ノ月", day: 2 },
+          dateOfBirth: { month: "笛鼠ノ月" },
           affiliations: ["ブレイバント", "dummy"],
           order: 1,
           memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
@@ -126,26 +137,10 @@ describe("migration-utils tests", () => {
         {
           id: "test-id-1",
           name: "オーパス",
-          dateOfBirth: { month: "天牛ノ月", day: 3 },
+          dateOfBirth: { day: 3 },
           affiliations: ["ブレイバント"],
           order: 1,
           memo: "大盾で仲間を守る冒険者。セトハお嬢に仕えていた元執事。",
-        },
-        {
-          id: "test-id-2",
-          name: "キサラギ",
-          dateOfBirth: { month: "王虎ノ月", day: 4 },
-          affiliations: ["ブレイバント"],
-          order: 1,
-          memo: "身の丈ほどの刀を振るう冒険者。中性的な雰囲気。",
-        },
-        {
-          id: "test-id-3",
-          name: "ヴィニヨン",
-          dateOfBirth: { month: "素兎ノ月", day: 5 },
-          affiliations: ["ブレイバント"],
-          order: 1,
-          memo: "魔法で仲間を支援する占師。訳アリの出自らしい。",
         },
       ];
 
@@ -156,7 +151,7 @@ describe("migration-utils tests", () => {
       expect(migratedEtrians[0]).toEqual({
         id: "test-id-0",
         name: "セトハ",
-        dateOfBirth: { month: "笛鼠ノ月", day: 2 }, // 更新対象
+        dateOfBirth: { month: "笛鼠ノ月", day: 1 }, // 更新対象
         affiliations: ["ブレイバント", "dummy"],
         order: 1,
         memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
@@ -164,26 +159,10 @@ describe("migration-utils tests", () => {
       expect(migratedEtrians[1]).toEqual({
         id: "test-id-1",
         name: "オーパス",
-        dateOfBirth: { month: "天牛ノ月", day: 3 }, // 更新対象
+        dateOfBirth: { month: "皇帝ノ月", day: 3 }, // 更新対象
         affiliations: ["ブレイバント"],
         order: 1,
         memo: "大盾で仲間を守る冒険者。セトハお嬢に仕えていた元執事。",
-      });
-      expect(migratedEtrians[2]).toEqual({
-        id: "test-id-2",
-        name: "キサラギ",
-        dateOfBirth: { month: "王虎ノ月", day: 4 }, // 更新対象
-        affiliations: ["ブレイバント"],
-        order: 1,
-        memo: "身の丈ほどの刀を振るう冒険者。中性的な雰囲気。",
-      });
-      expect(migratedEtrians[3]).toEqual({
-        id: "test-id-3",
-        name: "ヴィニヨン",
-        dateOfBirth: { month: "素兎ノ月", day: 5 }, // 更新対象
-        affiliations: ["ブレイバント"],
-        order: 1,
-        memo: "魔法で仲間を支援する占師。訳アリの出自らしい。",
       });
     });
   });
