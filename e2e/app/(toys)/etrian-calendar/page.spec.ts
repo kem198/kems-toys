bd097712b1ace2d70b0fd063babd11dc6d444cf0
KeM198 <kenkenpa198@gmail.com>
2025-11-08 12:10:54 +0900

    feat: refactor tests for Etrian calendar page to use dummy data setup in beforeEach and afterEach

--- e2e/app/(toys)/etrian-calendar/page.spec.ts
+++ e2e/app/(toys)/etrian-calendar/page.spec.ts
@@ -2,7 +2,39 @@ import { Etrian } from "@/app/(toys)/etrian-calendar/_common/types/etrian";
 import { ETRIAN_REGISTRY_STORAGE_KEY } from "@/app/(toys)/etrian-calendar/_features/registry/hooks/use-etrian-registry";
 import { expect, test } from "@playwright/test";
 
+const DUMMY_ETRIANS: Etrian[] = [
+  {
+    id: "dummy-etrian",
+    name: "dummy",
+    dateOfBirth: { month: "天牛ノ月", day: 1 },
+    affiliations: [],
+    order: 0,
+  },
+];
+
 test.describe("世界樹の暦ページのテスト", () => {
+  test.beforeEach(async ({ page }) => {
+    // ルートへ移動しておく
+    await page.goto("/");
+    // ダミー Etrians のセット
+    await page.evaluate(
+      ([key, value]) => {
+        localStorage.setItem(key, value);
+      },
+      [ETRIAN_REGISTRY_STORAGE_KEY, JSON.stringify(DUMMY_ETRIANS)],
+    );
+  });
+
+  test.afterEach(async ({ page }) => {
+    // ダミー Etrians を再度セット
+    await page.evaluate(
+      ([key, value]) => {
+        localStorage.setItem(key, value);
+      },
+      [ETRIAN_REGISTRY_STORAGE_KEY, JSON.stringify(DUMMY_ETRIANS)],
+    );
+  });
+
   test.describe("暦変換器 (太陽暦 -> 世界樹歴) のテスト", () => {
     test.describe("初期表示のテスト", () => {
       test("当日が '2025-01-01' の状態で、画面が初期表示された時、'2025-01-01' と '皇帝ノ月 1 日' が表示されること", async ({
@@ -10,14 +42,6 @@ test.describe("世界樹の暦ページのテスト", () => {
       }) => {
         // Arrange
         await page.clock.setFixedTime(new Date("2025-01-01T09:00:00"));
-        await page.goto("/");
-        const etrians: Etrian[] = [];
-        await page.evaluate(
-          ([key, value]) => {
-            localStorage.setItem(key, value);
-          },
-          [ETRIAN_REGISTRY_STORAGE_KEY, JSON.stringify(etrians)],
-        );
 
         // Act
         await page
@@ -34,22 +58,6 @@ test.describe("世界樹の暦ページのテスト", () => {
       }) => {
         // Arrange
         await page.clock.setFixedTime(new Date("2024-12-31T09:00:00"));
-        await page.goto("/");
-        const etrians: Etrian[] = [
-          {
-            id: "dummy-etrian",
-            name: "dummy",
-            dateOfBirth: { month: "天牛ノ月", day: 1 },
-            affiliations: [],
-            order: 0,
-          },
-        ];
-        await page.evaluate(
-          ([key, value]) => {
-            localStorage.setItem(key, value);
-          },
-          [ETRIAN_REGISTRY_STORAGE_KEY, JSON.stringify(etrians)],
-        );
 
         // Act
         await page
@@ -68,14 +76,6 @@ test.describe("世界樹の暦ページのテスト", () => {
       }) => {
         // Arrange
         await page.clock.setFixedTime(new Date("2025-01-01T09:00:00"));
-        await page.goto("/");
-        const etrians: Etrian[] = [];
-        await page.evaluate(
-          ([key, value]) => {
-            localStorage.setItem(key, value);
-          },
-          [ETRIAN_REGISTRY_STORAGE_KEY, JSON.stringify(etrians)],
-        );
         await page
           .getByRole("link", { name: "世界樹の暦 今日は何ノ月？" })
           .click();
@@ -97,14 +97,6 @@ test.describe("世界樹の暦ページのテスト", () => {
       }) => {
         // Arrange
         await page.clock.setFixedTime(new Date("2025-01-01T09:00:00"));
-        await page.goto("/");
-        const etrians: Etrian[] = [];
-        await page.evaluate(
-          ([key, value]) => {
-            localStorage.setItem(key, value);
-          },
-          [ETRIAN_REGISTRY_STORAGE_KEY, JSON.stringify(etrians)],
-        );
         await page
           .getByRole("link", { name: "世界樹の暦 今日は何ノ月？" })
           .click();
@@ -130,7 +122,6 @@ test.describe("世界樹の暦ページのテスト", () => {
         page,
       }) => {
         // Arrange
-        await page.goto("/");
         const etrians: Etrian[] = [
           {
             id: "test-etrian",
@@ -164,16 +155,13 @@ test.describe("世界樹の暦ページのテスト", () => {
         await expect(
           page.getByText("突剣を自在に扱う冒険者。没落貴族の一人娘。").first(),
         ).toBeVisible();
-
-        // Cleanup
-        await page.evaluate(
-          (key) => localStorage.removeItem(key),
-          ETRIAN_REGISTRY_STORAGE_KEY,
-        );
       });
     });
+
     test.describe.skip("作成時のテスト", () => {});
+
     test.describe.skip("更新時のテスト", () => {});
+
     test.describe.skip("削除時のテスト", () => {});
   });
 });
