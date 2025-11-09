67a85287504ef670be54151174c27ca5d0d3255a
KeM198 <kenkenpa198@gmail.com>
2025-11-09 23:52:19 +0900

    fix: refactor migration functions to include EtrianRegistry handling

--- src/app/(toys)/etrian-calendar/_features/registry/utils/migration-utils.ts
+++ src/app/(toys)/etrian-calendar/_features/registry/utils/migration-utils.ts
@@ -1,38 +1,40 @@
+import { CURRENT_ETRIAN_REGISTRY_VERSION } from "@/app/(toys)/etrian-calendar/_common/constants/date";
 import {
   Etrian,
   EtrianDateOfBirth,
+  EtrianRegistry,
   EtrianV1,
 } from "@/app/(toys)/etrian-calendar/_common/types/etrian";
 
-const migrateDateOfBirth = (
-  dateOfBirth: EtrianV1["dateOfBirth"],
-): EtrianDateOfBirth | undefined => {
-  if (dateOfBirth.month && dateOfBirth.day) {
-    return {
-      month: dateOfBirth.month,
-      day: dateOfBirth.day,
-    };
-  }
+const migrateEtriansV1toV2 = (etrianV1s: EtrianV1[]): Etrian[] => {
+  const migrateDateOfBirth = (
+    dateOfBirth: EtrianV1["dateOfBirth"],
+  ): EtrianDateOfBirth | undefined => {
+    if (dateOfBirth.month && dateOfBirth.day) {
+      return {
+        month: dateOfBirth.month,
+        day: dateOfBirth.day,
+      };
+    }
 
-  if (dateOfBirth.month && !dateOfBirth.day) {
-    return {
-      month: dateOfBirth.month,
-      day: 1,
-    };
-  }
+    if (dateOfBirth.month && !dateOfBirth.day) {
+      return {
+        month: dateOfBirth.month,
+        day: 1,
+      };
+    }
 
-  if (!dateOfBirth.month && dateOfBirth.day) {
-    return {
-      month: "皇帝ノ月",
-      day: dateOfBirth.day,
-    };
-  }
+    if (!dateOfBirth.month && dateOfBirth.day) {
+      return {
+        month: "皇帝ノ月",
+        day: dateOfBirth.day,
+      };
+    }
 
-  return undefined;
-};
+    return undefined;
+  };
 
-export const migrateEtriansV1toV2 = (etrianV1s: EtrianV1[]): Etrian[] =>
-  etrianV1s.map((etrianV1) => ({
+  return etrianV1s.map((etrianV1) => ({
     id: etrianV1.id,
     name: etrianV1.name,
     dateOfBirth: migrateDateOfBirth(etrianV1.dateOfBirth),
@@ -40,3 +42,28 @@ export const migrateEtriansV1toV2 = (etrianV1s: EtrianV1[]): Etrian[] =>
     order: etrianV1.order,
     memo: etrianV1.memo,
   }));
+};
+
+export const migrateEtrianRegistry = (
+  data: EtrianV1[] | EtrianRegistry,
+): EtrianRegistry => {
+  // EtrianRegistry 型の場合 (version を含む)
+  if ("version" in data) {
+    // すでに最新バージョンの場合はそのまま
+    if (data.version === CURRENT_ETRIAN_REGISTRY_VERSION) {
+      return data;
+    }
+
+    // 他のバージョンの場合は最新の JSON 形式に詰め替える (ここが後々の更新対象になるはず)
+    return {
+      version: CURRENT_ETRIAN_REGISTRY_VERSION,
+      etrians: data.etrians,
+    };
+  }
+
+  // EtrianV1[] は構造が一段階浅いので別処理で移行する
+  return {
+    version: CURRENT_ETRIAN_REGISTRY_VERSION,
+    etrians: migrateEtriansV1toV2(data),
+  };
+};
--- tests/app/(toys)/etrian-calendar/_features/registry/utils/migration-utils.test.ts
+++ tests/app/(toys)/etrian-calendar/_features/registry/utils/migration-utils.test.ts
@@ -1,168 +1,209 @@
-import { EtrianV1 } from "@/app/(toys)/etrian-calendar/_common/types/etrian";
-import { migrateEtriansV1toV2 } from "@/app/(toys)/etrian-calendar/_features/registry/utils/migration-utils";
+import {
+  EtrianRegistry,
+  EtrianV1,
+} from "@/app/(toys)/etrian-calendar/_common/types/etrian";
+import { migrateEtrianRegistry } from "@/app/(toys)/etrian-calendar/_features/registry/utils/migration-utils";
 import { describe, expect, it } from "vitest";
 
 describe("migration-utils tests", () => {
-  describe("migrateEtrians() tests", () => {
-    it("'EtrianV1[] が空配列のとき、Etrian[] の空配列を返すこと", () => {
-      // Arrange
-      const oldEtrians: EtrianV1[] = [];
+  describe("migrateToEtrianRegistry() tests", () => {
+    describe("EtrianV1 -> 最新の EtrianRegistry への移行", () => {
+      it("EtrianV1[] が渡された時、最新の EtrianRegistry へ変換されること (dateOfMonth.month/day あり -> そのまま)", () => {
+        // Arrange
+        const oldEtrians: EtrianV1[] = [
+          {
+            id: "test-id",
+            name: "セトハ",
+            dateOfBirth: { month: "怒猪ノ月", day: 28 },
+            affiliations: ["ブレイバント"],
+            order: 1,
+            memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+          },
+        ];
 
-      // Act
-      const migratedEtrians = migrateEtriansV1toV2(oldEtrians);
+        // Act
+        const registry = migrateEtrianRegistry(oldEtrians);
 
-      // Assert
-      expect(migratedEtrians).toEqual([]);
-    });
-
-    it("'EtrianV1[] を渡すと Etrian[] へ変換されること (dateOfMonth.month/day あり -> そのまま)", () => {
-      // Arrange
-      const oldEtrians: EtrianV1[] = [
-        {
-          id: "test-id",
-          name: "セトハ",
-          dateOfBirth: { month: "怒猪ノ月", day: 28 },
-          affiliations: ["ブレイバント"],
-          order: 1,
-          memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
-        },
-      ];
-
-      // Act
-      const migratedEtrians = migrateEtriansV1toV2(oldEtrians);
-
-      // Assert
-      expect(migratedEtrians[0]).toEqual({
-        id: "test-id",
-        name: "セトハ",
-        dateOfBirth: { month: "怒猪ノ月", day: 28 }, // 更新対象
-        affiliations: ["ブレイバント"],
-        order: 1,
-        memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+        // Assert
+        expect(registry.version).toBe(2);
+        expect(registry.etrians).toEqual([
+          {
+            id: "test-id",
+            name: "セトハ",
+            dateOfBirth: { month: "怒猪ノ月", day: 28 },
+            affiliations: ["ブレイバント"],
+            order: 1,
+            memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+          },
+        ]);
       });
-    });
 
-    it("'EtrianV1[] を渡すと Etrian[] へ変換されること (dateOfMonth.day なし -> dateOfMonth.day に 1)", () => {
-      // Arrange
-      const oldEtrians: EtrianV1[] = [
-        {
-          id: "test-id",
-          name: "セトハ",
-          dateOfBirth: { month: "怒猪ノ月" },
-          affiliations: ["ブレイバント"],
-          order: 1,
-          memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
-        },
-      ];
+      it("EtrianV1[] が渡された時、最新の EtrianRegistry へ変換されること (dateOfMonth.day なし -> dateOfMonth.day に 1)", () => {
+        // Arrange
+        const oldEtrians: EtrianV1[] = [
+          {
+            id: "test-id",
+            name: "セトハ",
+            dateOfBirth: { month: "怒猪ノ月" },
+            affiliations: ["ブレイバント"],
+            order: 1,
+            memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+          },
+        ];
 
-      // Act
-      const migratedEtrians = migrateEtriansV1toV2(oldEtrians);
+        // Act
+        const registry = migrateEtrianRegistry(oldEtrians);
 
-      // Assert
-      expect(migratedEtrians[0]).toEqual({
-        id: "test-id",
-        name: "セトハ",
-        dateOfBirth: { month: "怒猪ノ月", day: 1 }, // 更新対象
-        affiliations: ["ブレイバント"],
-        order: 1,
-        memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+        // Assert
+        expect(registry.version).toBe(2);
+        expect(registry.etrians).toEqual([
+          {
+            id: "test-id",
+            name: "セトハ",
+            dateOfBirth: { month: "怒猪ノ月", day: 1 },
+            affiliations: ["ブレイバント"],
+            order: 1,
+            memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+          },
+        ]);
       });
-    });
 
-    it("'EtrianV1[] を渡すと Etrian[] へ変換されること (dateOfMonth.month なし -> dateOfMonth.month に'皇帝ノ月')", () => {
-      // Arrange
-      const oldEtrians: EtrianV1[] = [
-        {
-          id: "test-id",
-          name: "セトハ",
-          dateOfBirth: { day: 28 },
-          affiliations: ["ブレイバント"],
-          order: 1,
-          memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
-        },
-      ];
+      it("EtrianV1[] が渡された時、最新の EtrianRegistry へ変換されること (dateOfMonth.month なし -> dateOfMonth.month に'皇帝ノ月')", () => {
+        // Arrange
+        const oldEtrians: EtrianV1[] = [
+          {
+            id: "test-id",
+            name: "セトハ",
+            dateOfBirth: { day: 28 },
+            affiliations: ["ブレイバント"],
+            order: 1,
+            memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+          },
+        ];
 
-      // Act
-      const migratedEtrians = migrateEtriansV1toV2(oldEtrians);
+        // Act
+        const registry = migrateEtrianRegistry(oldEtrians);
 
-      // Assert
-      expect(migratedEtrians[0]).toEqual({
-        id: "test-id",
-        name: "セトハ",
-        dateOfBirth: { month: "皇帝ノ月", day: 28 }, // 更新対象
-        affiliations: ["ブレイバント"],
-        order: 1,
-        memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+        // Assert
+        expect(registry.version).toBe(2);
+        expect(registry.etrians).toEqual([
+          {
+            id: "test-id",
+            name: "セトハ",
+            dateOfBirth: { month: "皇帝ノ月", day: 28 },
+            affiliations: ["ブレイバント"],
+            order: 1,
+            memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+          },
+        ]);
       });
-    });
 
-    it("'EtrianV1[] を渡すと Etrian[] へ変換されること (dateOfMonth.month/day なし -> dateOfMonth なし)", () => {
-      // Arrange
-      const oldEtrians: EtrianV1[] = [
-        {
-          id: "test-id",
-          name: "セトハ",
-          dateOfBirth: {},
-          affiliations: ["ブレイバント"],
-          order: 1,
-          memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
-        },
-      ];
+      it("EtrianV1[] が渡された時、最新の EtrianRegistry へ変換されること (dateOfMonth.month/day なし -> dateOfMonth なし)", () => {
+        // Arrange
+        const oldEtrians: EtrianV1[] = [
+          {
+            id: "test-id",
+            name: "セトハ",
+            dateOfBirth: {},
+            affiliations: ["ブレイバント"],
+            order: 1,
+            memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+          },
+        ];
 
-      // Act
-      const migratedEtrians = migrateEtriansV1toV2(oldEtrians);
+        // Act
+        const registry = migrateEtrianRegistry(oldEtrians);
 
-      // Assert
-      expect(migratedEtrians[0]).toEqual({
-        id: "test-id",
-        name: "セトハ",
-        dateOfBirth: undefined, // 更新対象
-        affiliations: ["ブレイバント"],
-        order: 1,
-        memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+        // Assert
+        expect(registry.version).toBe(2);
+        expect(registry.etrians).toEqual([
+          {
+            id: "test-id",
+            name: "セトハ",
+            dateOfBirth: undefined,
+            affiliations: ["ブレイバント"],
+            order: 1,
+            memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+          },
+        ]);
       });
-    });
 
-    it("'EtrianV1[] を渡すと Etrian[] へ変換されること (配列)", () => {
-      // Arrange
-      const oldEtrians: EtrianV1[] = [
-        {
-          id: "test-id-0",
-          name: "セトハ",
-          dateOfBirth: { month: "笛鼠ノ月" },
-          affiliations: ["ブレイバント", "dummy"],
-          order: 1,
-          memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
-        },
-        {
-          id: "test-id-1",
-          name: "オーパス",
-          dateOfBirth: { day: 3 },
-          affiliations: ["ブレイバント"],
-          order: 1,
-          memo: "大盾で仲間を守る冒険者。セトハお嬢に仕えていた元執事。",
-        },
-      ];
+      it("空の EtrianV1[] が渡された時、空の etrians を持つ EtrianRegistry へ変換されること", () => {
+        // Arrange
+        const oldEtrians: EtrianV1[] = [];
 
-      // Act
-      const migratedEtrians = migrateEtriansV1toV2(oldEtrians);
+        // Act
+        const registry = migrateEtrianRegistry(oldEtrians);
 
-      // Assert
-      expect(migratedEtrians[0]).toEqual({
-        id: "test-id-0",
-        name: "セトハ",
-        dateOfBirth: { month: "笛鼠ノ月", day: 1 }, // 更新対象
-        affiliations: ["ブレイバント", "dummy"],
-        order: 1,
-        memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+        // Assert
+        expect(registry.version).toBe(2);
+        expect(registry.etrians).toEqual([]);
       });
-      expect(migratedEtrians[1]).toEqual({
-        id: "test-id-1",
-        name: "オーパス",
-        dateOfBirth: { month: "皇帝ノ月", day: 3 }, // 更新対象
-        affiliations: ["ブレイバント"],
-        order: 1,
-        memo: "大盾で仲間を守る冒険者。セトハお嬢に仕えていた元執事。",
+
+      it("EtrianRegistry (v1) が渡された時、最新の EtrianRegistry へ変換されること", () => {
+        // Arrange
+        const oldRegistry: EtrianRegistry = {
+          version: 1,
+          etrians: [
+            {
+              id: "test-id",
+              name: "セトハ",
+              dateOfBirth: { month: "怒猪ノ月", day: 28 },
+              affiliations: ["ブレイバント"],
+              order: 1,
+              memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+            },
+          ],
+        };
+
+        // Act
+        const registry = migrateEtrianRegistry(oldRegistry);
+
+        // Assert
+        expect(registry.version).toBe(2);
+        expect(registry.etrians).toEqual([
+          {
+            id: "test-id",
+            name: "セトハ",
+            dateOfBirth: { month: "怒猪ノ月", day: 28 },
+            affiliations: ["ブレイバント"],
+            order: 1,
+            memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+          },
+        ]);
+      });
+
+      it("最新の EtrianRegistry が渡された時、そのまま返すこと", () => {
+        // Arrange
+        const currentRegistry: EtrianRegistry = {
+          version: 2,
+          etrians: [
+            {
+              id: "test-id",
+              name: "セトハ",
+              dateOfBirth: { month: "怒猪ノ月", day: 28 },
+              affiliations: ["ブレイバント"],
+              order: 1,
+              memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+            },
+          ],
+        };
+
+        // Act
+        const registry = migrateEtrianRegistry(currentRegistry);
+
+        // Assert
+        expect(registry.version).toBe(2);
+        expect(registry.etrians).toEqual([
+          {
+            id: "test-id",
+            name: "セトハ",
+            dateOfBirth: { month: "怒猪ノ月", day: 28 },
+            affiliations: ["ブレイバント"],
+            order: 1,
+            memo: "突剣を自在に扱う冒険者。没落貴族の一人娘。",
+          },
+        ]);
       });
     });
   });
